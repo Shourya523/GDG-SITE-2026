20665554292eaace3d0ac3a92bdbb512d3556676
Shourya Singh <shourya124sensei@gmail.com>
2026-01-03 15:44:05 +0530

    added event pages route and subroutes

--- src/app/events/layout.tsx
+++ src/app/events/layout.tsx
@@ -1,14 +1,18 @@
+import type { Metadata } from "next"; 
 import EventsBottomNavbar from "@/src/component/EventsBottomNavBar";
-
+export const metadata: Metadata = {
+    title: "GDG Events",
+    description: "From beginner sessions to hackathons—explore the events where the GDG community learns, builds, and connects.",
+};
 export default function EventsLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-    <section className="events-layout">
-      {children}
-      <EventsBottomNavbar />
-    </section>
-  );
+    return (
+        <section className="events-layout">
+            {children}
+            <EventsBottomNavbar />
+        </section>
+    );
 }
--- src/app/team/layout.tsx
+++ src/app/team/layout.tsx
@@ -3,7 +3,7 @@ import Navbar from "@/src/component/Navbar";
 import Footer from "@/src/component/Footer";
 
 export const metadata: Metadata = {
-  title: "GDG Team | Meet the Builders",
+  title: "GDG Team | Where Builders Come Together",
   description: "Meet the Mentors, Team Leads, and Core Team behind GDG.",
 };
 
